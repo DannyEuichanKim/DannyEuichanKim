60187c9a9f703702e957e913b5ba24a6ce5a199b
Danny Kim <dannykim1130@uchicago.edu>
2020-03-06 04:14:01 -0600

    bare bones code

---
+++ calming-confidential/src/containers/App/firebase-config.js
@@ -0,0 +1,11 @@
+
+// App/firebase-config.js
+
+export default {
+  apiKey: "AIzaSyBD7AJRBj1MMqsVOEFBXT7O5X3feS_tPkk",
+  authDomain: "reddit-clone-7bfd4.firebaseapp.com",
+  databaseURL: "https://reddit-clone-7bfd4.firebaseio.com",
+  projectId: "reddit-clone-7bfd4",
+  storageBucket: "reddit-clone-7bfd4.appspot.com",
+  messagingSenderId: "768252708505"
+};
