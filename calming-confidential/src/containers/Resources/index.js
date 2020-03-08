1a02462c5688b15c31252624e919f39b1584eb97
Lucas Du <lucasdu@Lucass-MacBook-Pro-4.local>
2020-03-08 14:31:35 -0500

    create skeleton for Resources

---
+++ calming-confidential/src/containers/Resources/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Resources extends Component {
+	render() {
+		return (
+			<div class = "title"><h1>List of resources:</h1>
+				<a href="https://wellness.uchicago.edu/about/appointments/">Schedule an appointment</    a>: Can schedule through phone or in person.
+				<a href="https://wellness.uchicago.edu/mental-health/lets-talk/">Let's Talk</a>: Informal walk-in sessions with SCS staff.
+				<a href="https://wellness.uchicago.edu/mental-health/referrals-from-scs/">SCS referrals</a>: For therapists outside of UChicago
+			</div>
+		//  <script type="text/javascript" src="scripts/script.js"></script>
+		// html goes here (make one big overarching div)
+		);
+	}
+}
+
+export default Resources;
