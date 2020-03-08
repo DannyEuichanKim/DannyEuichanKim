178a5e112e3108aa92e0d8681eb5650775421b4e
razor4143 <oliveryzhao@gmail.com>
2020-03-08 14:00:26 -0500

    css changes

--- calming-confidential/src/containers/Posts/index.js
+++ calming-confidential/src/containers/Posts/index.js
@@ -21,6 +21,15 @@ class Posts extends Component {
     });
   }
 
+  handleComment = (post, key) => {
+    this.props.firebase.ref('posts/' + key).set({
+      title: post.title,
+      upvote: post.upvote,
+      downvote: post.downvote + 1,
+      content: post.content
+    });
+  }
+
   render() {
     let posts = this.props.posts;
     let _this = this;
@@ -45,39 +54,45 @@ class Posts extends Component {
           <a href="/pinned"><button>Pinned Posts</button></a>
         </div>
 
-        <div>
+        
         {
           Object.keys(posts).reverse().map(function(key) {
             return (
               <div id ="scr" class="container">
-              <div id="post1" class="post" onclick="">
-              
-              <div key={key}>
-                <div>Title: { posts[key].title }</div>
-                <div>Content: { posts[key].content }</div>
-                <div>Agrees: { posts[key].upvote }</div>
-                <div>
-                  <button
-                    onClick={ _this.handleUpvote.bind(this, posts[key], key) }
-                    type="button"
-                  >
-                     Agree
-                  </button>
+                <br />
+                <div id="post1" class="post" onclick="">
+                  <div class="title" key={key}>
+                      <div>Title: { posts[key].title }</div>
+                  </div>
+                  <div class="text" key={key}>
+                      <div>Content: { posts[key].content }</div>
+                  </div>
+                  <div class="agrees" key={key}>
+                      <div>Agrees: { posts[key].upvote }</div>
+                  </div>
+                  <div class="menu" key={key}>
+                    <div>
+                      <button
+                        onClick={ _this.handleUpvote.bind(this, posts[key], key) }
+                        type="button"
+                      >
+                         Agree
+                      </button>
 
-                  <button
-                    type="button"
-                  >
-                    Add Comment
-                  </button>
+                      <button
+                        type="button"
+                      >
+                        Add Comment
+                      </button>
+                    </div>
+                  </div>
                 </div>
               </div>
 
-              </div>
-              </div>
             );
           })
         }
-        </div>
+        
       </div>
     );
   }
