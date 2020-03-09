a7fc3d1f077bdb1b429b7ae09913f0b17563c33c
Lucas Du <lucasdu@Lucass-MacBook-Pro-4.local>
2020-03-08 21:29:17 -0500

    changed button class name

--- calming-confidential/src/containers/Resources/index.js
+++ calming-confidential/src/containers/Resources/index.js
@@ -1,39 +1,42 @@
 import React, { Component } from 'react';
+import Navbar from '../Navbar/index.js';
+import './style.css';
 // import Card from "card";
 
 class Resources extends Component {
 	render() {
 		return (
 			<>
-				<div class = "content" style={{marginLeft: "25%", marginRight: "25%",
-											   marginTop: "10%"}}>
-					<div class = "title" style = {{backgroundColor: "#0080FF", textAlign: "center"}}>
-						<h1 style={{fontSize: "64px", fontWeight: "bold"}}>List of resources</h1>
+				<Navbar />
+				<div class = "content">
+					<div class = "title1">
+						<h1>List of resources</h1>
 					</div>
 					<div class = "body">
 						<div>
 							<a href="https://wellness.uchicago.edu/about/appointments/">
-								<button style={{width: "100%", height: "100px", fontSize: "40px", backgroundColor: "#FFD300"}}>
+								<button class = "button1"> 
 									Schedule an appointment with SCS
 								</button>
 							</a>
 						</div>
 						<div>
 							<a href="https://wellness.uchicago.edu/mental-health/referrals-from-scs/">
-								<button style={{width: "100%", height: "100px", fontSize: "48px", backgroundColor: "#FFD300"}}>
+								<button class = "button1">
 									Get an SCS referral
 								</button>
 							</a>
 						</div>
 						<div>
 							<a href="https://wellness.uchicago.edu/mental-health/lets-talk/">
-								<button style={{width: "100%", height: "100px", fontSize: "48px", backgroundColor: "#FFD300"}}>
+								<button class = "button1"> 
 									Learn more about Let's Talk
 								</button>
 							</a>
 						</div>
 						<div>
-							<a href="https://lean0n.me/uchicago/"><button style={{width: "100%", height: "100px", fontSize: "48px", backgroundColor: "#FFD300"}}>
+							<a href="https://lean0n.me/uchicago/">
+								<button class = "button1">
 									Contact Lean on Me
 								</button>
 							</a>
