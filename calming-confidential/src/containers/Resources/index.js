e666bc38c6e3e8718f6ef7163377c556fddbad92
Lucas Du <lucasdu@Lucass-MacBook-Pro-4.local>
2020-03-08 20:02:49 -0500

    created Resources page

--- calming-confidential/src/containers/Resources/index.js
+++ calming-confidential/src/containers/Resources/index.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react';
+// import Card from "card";
 
 class Resources extends Component {
 	render() {
 		return (
-			<div class = "title"><h1>List of resources:</h1>
-				<a href="https://wellness.uchicago.edu/about/appointments/">Schedule an appointment</    a>: Can schedule through phone or in person.
-				<a href="https://wellness.uchicago.edu/mental-health/lets-talk/">Let's Talk</a>: Informal walk-in sessions with SCS staff.
-				<a href="https://wellness.uchicago.edu/mental-health/referrals-from-scs/">SCS referrals</a>: For therapists outside of UChicago
-			</div>
+			<>
+				<div class = "content" style={{marginLeft: "25%", marginRight: "25%",
+											   marginTop: "10%"}}>
+					<div class = "title" style = {{backgroundColor: "#0080FF", textAlign: "center"}}>
+						<h1 style={{fontSize: "64px", fontWeight: "bold"}}>List of resources</h1>
+					</div>
+					<div class = "body">
+						<div>
+							<a href="https://wellness.uchicago.edu/about/appointments/">
+								<button style={{width: "100%", height: "100px", fontSize: "40px", backgroundColor: "#FFD300"}}>
+									Schedule an appointment with SCS
+								</button>
+							</a>
+						</div>
+						<div>
+							<a href="https://wellness.uchicago.edu/mental-health/referrals-from-scs/">
+								<button style={{width: "100%", height: "100px", fontSize: "48px", backgroundColor: "#FFD300"}}>
+									Get an SCS referral
+								</button>
+							</a>
+						</div>
+						<div>
+							<a href="https://wellness.uchicago.edu/mental-health/lets-talk/">
+								<button style={{width: "100%", height: "100px", fontSize: "48px", backgroundColor: "#FFD300"}}>
+									Learn more about Let's Talk
+								</button>
+							</a>
+						</div>
+						<div>
+							<a href="https://lean0n.me/uchicago/"><button style={{width: "100%", height: "100px", fontSize: "48px", backgroundColor: "#FFD300"}}>
+									Contact Lean on Me
+								</button>
+							</a>
+						</div>
+					</div>
+				</div>
+			</>
+//					<a href="https://wellness.uchicago.edu/mental-health/lets-talk/">Let's Talk</a>: Informal walk-in sessions with SCS staff.
+//					<br/>
+//					<a href="https://wellness.uchicago.edu/mental-health/referrals-from-scs/">SCS referrals</a>: For therapists outside of UChicago
+//				</div>
 		//  <script type="text/javascript" src="scripts/script.js"></script>
 		// html goes here (make one big overarching div)
 		);
