28cac2467319a929ea7db16ccd78d8a59d7e4bd7
thoranderskow <tanderskow@gmail.com>
2020-03-08 21:55:21 -0500

    finished

--- calming-confidential/src/containers/Pinned/index.js
+++ calming-confidential/src/containers/Pinned/index.js
@@ -10,11 +10,11 @@ class Pinned extends Component {
     return (
       <div className="Posts">
         <Navbar />
-        <div id ="scr" class="container">
-        <h1>Pinned Posts</h1>
-          <ul>
+        <div id ="scr" class="container"
+        style={{marginLeft: "15%", marginTop: "5%"}}>
+        <h1 style={{color: "black"}}>Pinned Posts</h1>
+          <ul style={{width: "100%", textAlign: "left"}}>
             <li>
-              <img src={logo} />
               <a href="https://lean0n.me/">
                 <h3>Reminder: You can talk with someone right now on Lean On Me!</h3>
                 <p>Confident Confidential is great, but if you need a more tailored, consistent...</p>
@@ -22,13 +22,11 @@ class Pinned extends Component {
             </li>
 
             <li>
-              <img src={logo} />
               <h3>Weekly Events Discussion Page</h3>
               <p>Use this thread as a place to.... </p>
             </li>
 
             <li>
-              <img src={logo} />
               <a href="https://www.classes.cs.uchicago.edu/archive/2020/winter/20370-1/">
                 <h3>Visionary new students in Inclusive Tech Class create helpful website for those struggling with mental health at UChicago</h3>
                 <p>What do you guys think about this idea? Nothing could compare to Confident Confidential</p>
